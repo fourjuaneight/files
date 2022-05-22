5f5b6e00920a35028df5f9547a9dcea78cc33af1
Juan Villela <juan@villela.co>
2022-05-21 22:17:53 -0400

    feat: Add B2 upload types.

---
+++ src/typings.d.ts
@@ -0,0 +1,66 @@
+/* eslint-disable camelcase */
+export interface RequestFormData {
+  key: string;
+  name: string;
+  ext: string;
+  file: Blob;
+}
+
+// B2
+export interface B2AuthResp {
+  absoluteMinimumPartSize: number;
+  accountId: string;
+  allowed: {
+    bucketId: string;
+    bucketName: string;
+    capabilities: string[];
+    namePrefix: null;
+  };
+  apiUrl: string;
+  authorizationToken: string;
+  downloadUrl: string;
+  recommendedPartSize: number;
+  s3ApiUrl: String;
+}
+
+export interface B2UpUrlResp {
+  bucketId: string;
+  uploadUrl: string;
+  authorizationToken: string;
+}
+
+export interface B2UploadResp {
+  fileId: string;
+  fileName: string;
+  accountId: string;
+  bucketId: string;
+  contentLength: number;
+  contentSha1: string;
+  contentType: string;
+  fileInfo: {
+    author: string;
+  };
+  serverSideEncryption: {
+    algorithm: string;
+    mode: string;
+  };
+}
+
+export interface B2Error {
+  status: number;
+  code: string;
+  message: string;
+}
+
+export interface B2AuthTokens {
+  apiUrl: string;
+  authorizationToken: string;
+  downloadUrl: string;
+  recommendedPartSize: number;
+}
+
+export interface B2UploadTokens {
+  endpoint: string;
+  authToken: string;
+  downloadUrl: string;
+}
